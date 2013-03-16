16373c7d3b77512387af42a99b243075d677a3ce
tzewang <tzewang@tzewang-desktop.(none)>
2013-03-16 16:54:16 +0000

    More test content added; main content templated into separate views; social media sharing added

--- cms/views.py
+++ cms/views.py
@@ -10,11 +10,22 @@ def article(request, title):
     partners = Partner.objects.select_related().all()
     article = Article.objects.select_related().get(title_url=title)
     recentStories = Article.objects.select_related().exclude(title_url=title).order_by('date_published')
-    template = loader.get_template('cms/article.html')
+    photoStories = None
+    if article.article_type == "text":
+        template = loader.get_template('cms/article_text.html')
+    elif article.article_type == "photo":
+        template = loader.get_template('cms/article_photo.html')
+        photoStories = Article.objects.select_related().all().filter(article_type="photo")
+    elif article.article_type == "youtube":
+        template = loader.get_template('cms/article_youtube.html')
+    else:
+        # something went wrong! Raise exception?
+        pass
     context = RequestContext(request, {
         'partners': partners,
         'article': article,
-        'recentStories': recentStories
+        'recentStories': recentStories,
+        'photoStories': photoStories,
     })
     return HttpResponse(template.render(context))
 
