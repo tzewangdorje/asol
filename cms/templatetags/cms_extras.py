d9974265450f394d31af5e5a6dfec237860bef4d
tzewang <tzewang@tzewang-desktop.(none)>
2013-04-06 20:46:23 +0100

    got the partner media icons working properly

--- cms/templatetags/cms_extras.py
+++ cms/templatetags/cms_extras.py
@@ -11,4 +11,22 @@ def makeUrl(article):
 def tel(number):
     return re.sub('[^0-9]', '', number)
 
+@register.filter(name='icon')
+def icon(type):
+    if type == "twitter":
+        return "social foundicon-twitter"
+    if type == "facebook":
+        return "social foundicon-facebook"
+    elif type == "email":
+        return "gen-enclosed foundicon-mail"
+    elif type == "facebook":
+        return "gen-enclosed foundicon-idea"
+    elif type == "website":
+        return "gen-enclosed foundicon-website"
+    elif type == "phone":
+        return "gen-enclosed foundicon-phone"
+    else:
+        return "gen-enclosed foundicon-idea"
+    
+
 
